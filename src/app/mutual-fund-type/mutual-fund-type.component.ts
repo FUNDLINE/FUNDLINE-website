200c7e64371c4703f32ec817efe2eafcae86f2a2
G-Basak <basak.gunjan@gmail.com>
2021-06-12 21:18:30 +0530

    Router added

---
+++ src/app/mutual-fund-type/mutual-fund-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mutual-fund-type',
+  templateUrl: './mutual-fund-type.component.html',
+  styleUrls: ['./mutual-fund-type.component.css']
+})
+export class MutualFundTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
