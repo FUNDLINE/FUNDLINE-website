0d00a619e34a5cba5e7eb9560951a10410b6ffd8
G-Basak <basak.gunjan@gmail.com>
2021-07-11 21:15:12 +0530

    Redesign

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  certList: Certified[];
+
+  constructor() {
+    this.certList = [
+      new Certified("Insurance Regulatory and Development Authority", "IRDA", "../../assets/home/irda_logo.png"),
+      new Certified("Assosiation of Mutual Fund in India", "AMFI", "../../assets/home/amfi_logo.png")
+    ]
+  }
 
   ngOnInit(): void {
   }
 
+}
+
+class Certified {
+
+  constructor(
+    private name:string,
+    private altName:string,
+    private image:string) {
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getImage(): string {
+    return this.image;
+  }
+
+  public getAltName(): string {
+    return this.image;
+  }
 }
