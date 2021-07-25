64b1ac371a9f7727f0abdfcc5a0923363bfb2e2f
G-Basak <basak.gunjan@gmail.com>
2021-07-25 20:44:34 +0530

    Added more mutual fund logo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,28 +18,52 @@ export class HomeComponent implements OnInit {
 
     this.mutualFundList = [
       Image.createImageWithBgColor("Axis Mutual Fund", "axis-mutual-fund-logo", "../../assets/home/mutual-fund-logo/axis_logo.svg", "#b5245d"),
-      Image.createImage("LIC Mutual Fund", "lic-mutual-fund-logo", "../../assets/home/mutual-fund-logo/LIC-Logo.png"),
-      Image.createImage("", "", "../../assets/home/mutual-fund-logo/uti_logo.png"),
-
-      Image.createImage("", "", "../../assets/home/mutual-fund-logo/yes-mf-logo.png"),
-      Image.createImageWithBgColor("", "", "../../assets/home/mutual-fund-logo/aditya-birla-capital-logo.png", "#c7222a"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/sbi-mf-logo.png"),
       Image.createImage("", "", "../../assets/home/mutual-fund-logo/icici-logo.png"),
 
       Image.createImageWithBgColor("", "", "../../assets/home/mutual-fund-logo/KotakLogo.svg", "#da251c"),
-      Image.createImage("", "", "../../assets/home/mutual-fund-logo/mahindra-mf-logo.jpg"),
-      Image.createImage("", "", "../../assets/home/mutual-fund-logo/hdfc_logo.svg"),
-
-      Image.createImage("", "", "../../assets/home/mutual-fund-logo/sbi-mf-logo.png"),
       Image.createImage("", "", "../../assets/home/mutual-fund-logo/tata-mf-logo.png"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/Canara-Robeco-Logo.svg"),
+
+
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/hdfc_logo.svg"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/parag-parikh-logo.png"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/pgim-logo.svg"),
+
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/franklin-templation-investment_logo.webp"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/reliance-nippon-mutual-fund.png"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/uti_logo.png"),
+
+      Image.createImageWithBgColor("", "", "../../assets/home/mutual-fund-logo/aditya-birla-capital-logo.png", "#c7222a"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/mirae-asset-mutual-fund-logo.jpg"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/invesco_mf_logo.jpg"),
+
+      Image.createImageWithBgColor("", "", "../../assets/home/mutual-fund-logo/ltfs-logo.svg", "#ffcb05"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/LIC-Logo.png"),
+      Image.createImageWithBgColor("", "", "../../assets/home/mutual-fund-logo/edelweiss-logo.png", "#2857a4"),
+
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/motilal-oswal-amc-logo.png"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/IDBI-MF-Logo.jpg"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/dsp-logo.png"),
+
       Image.createImage("", "", "../../assets/home/mutual-fund-logo/Union-MF-Logo.jpg"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/boi-mf-logo.png"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/quant-logo.png"),
+
+      Image.createImageWithBgColor("", "", "../../assets/home/mutual-fund-logo/quantum-logo.png", "#0f376a"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/principal-logo.png"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/bnp-paribas-logo.jpg"),
 
       Image.createImage("", "", "../../assets/home/mutual-fund-logo/iti-mf-logo.png"),
-      Image.createImage("", "", "../../assets/home/mutual-fund-logo/Canara-Robeco-Logo.svg"),
-      Image.createImage("", "", "../../assets/home/mutual-fund-logo/franklin-templation-investment_logo.webp"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/yes-mf-logo.png"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/navi-logo.svg"),
 
-      Image.createImage("", "", "../../assets/home/mutual-fund-logo/boi-mf-logo.png"),
-      Image.createImageWithBgColor("", "", "../../assets/home/mutual-fund-logo/ltfs-logo.svg", "#ffcb05"),
-      Image.createImage("", "", "../../assets/home/mutual-fund-logo/IDBI-MF-Logo.jpg")
+
+      //religare mf
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/mahindra-mf-logo.jpg"),
+      Image.createImage("", "", "../../assets/home/mutual-fund-logo/trust-logo.png")
+
+
     ]
   }
 
