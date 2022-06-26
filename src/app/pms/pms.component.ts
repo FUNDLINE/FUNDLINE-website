d0368cdac1e0d90f12be4c85716a85760a957644
G-Basak <basak.gunjan@gmail.com>
2022-06-26 23:17:08 +0530

    Added PMS section.

---
+++ src/app/pms/pms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pms',
+  templateUrl: './pms.component.html',
+  styleUrls: ['./pms.component.css']
+})
+export class PMSComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
